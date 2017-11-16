8b077a0fd795d60f9a10f7e16cc0863dc213e370
DanaMonica <monicatanase96@yahoo.com>
2017-11-16 09:31:33 +0200

    Solve colors

--- cmake_install.cmake
+++ cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/cloud/ProiectFIC/Temp
+# Install script for directory: /home/cloud/Proiect/Temp
 
 # Set the install prefix
 IF(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -38,7 +38,7 @@ ELSE(CMAKE_INSTALL_COMPONENT)
   SET(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
 ENDIF(CMAKE_INSTALL_COMPONENT)
 
-FILE(WRITE "/home/cloud/ProiectFIC/Temp/${CMAKE_INSTALL_MANIFEST}" "")
+FILE(WRITE "/home/cloud/Proiect/Temp/${CMAKE_INSTALL_MANIFEST}" "")
 FOREACH(file ${CMAKE_INSTALL_MANIFEST_FILES})
-  FILE(APPEND "/home/cloud/ProiectFIC/Temp/${CMAKE_INSTALL_MANIFEST}" "${file}\n")
+  FILE(APPEND "/home/cloud/Proiect/Temp/${CMAKE_INSTALL_MANIFEST}" "${file}\n")
 ENDFOREACH(file)
